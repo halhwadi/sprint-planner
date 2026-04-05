d29ea388c903ff4076b10ac7cae54e2d3e8f983f
Husam Alhwadi <halhwadi@yahoo.com>
2026-04-05 16:59:17 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
+python manage.py makemigrations --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py shell -c "
