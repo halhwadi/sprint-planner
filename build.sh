27c3573259150f6fd0b464eebc122f2a4cf0953d
Husam Alhwadi <halhwadi@yahoo.com>
2026-04-06 22:42:17 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py makemigrations --noinput
 python manage.py collectstatic --noinput
+python manage.py migrate planner --fake
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
