e1ce4c697d01f9c4032f7f16a2b67b19eb97985a
Husam Alhwadi <halhwadi@yahoo.com>
2026-04-06 22:20:41 +0400

    Update models.py

--- planner/models.py
+++ planner/models.py
@@ -158,7 +158,7 @@ class Sprint(models.Model):
 class SprintMember(models.Model):
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='sprint_members', null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sprint_memberships', null=True, blank=True)
-    stream       = models.ForeignKey(Stream, on_delete=models.SET_NULL, null=True, blank=True, related_name='members')
+    stream       = models.CharField(max_length=20, blank=True)
     is_active    = models.BooleanField(default=True)
     created_at   = models.DateTimeField(auto_now_add=True)
 
