eaebd948b30803df84bd40d9349d6970552321c2
Husam Alhwadi <halhwadi@yahoo.com>
2026-04-10 22:17:06 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
-python manage.py makemigrations planner
+
+# Delete old migrations and regenerate fresh
+find . -path "*/planner/migrations/0*.py" -delete
+python manage.py makemigrations planner --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate
+
 python manage.py shell -c "
 from django.contrib.auth.models import User
 from planner.models import Organization, Subscription, OrganizationMember
 from django.utils import timezone
 from datetime import timedelta
-from django.utils.text import slugify
 
-# Create superuser if not exists
 if not User.objects.filter(username='scrummaster').exists():
     user = User.objects.create_superuser('scrummaster', 'admin@sprintflow.co', 'Sprint@2024')
     print('Superuser created')
@@ -19,7 +21,6 @@ else:
     user = User.objects.get(username='scrummaster')
     print('Superuser already exists')
 
-# Create default org if not exists
 if not Organization.objects.exists():
     org = Organization.objects.create(
         name='SprintFlow',
